88dbfb4ffe681cb4946d435e0c76f0bd9c73f46b
Konstantin8105 <konstantin8105@gmail.com>
2023-05-18 14:41:24 +0300

    add bench

--- efmt_test.go
+++ efmt_test.go
@@ -25,3 +25,20 @@ func Test(t *testing.T) {
 	}
 	compare.Test(t, ".fts", buf.Bytes())
 }
+
+// cpu: Intel(R) Xeon(R) CPU E3-1240 V2 @ 3.40GHz
+// Benchmark/-011-4         	 1393530	       843.0 ns/op	      48 B/op	       4 allocs/op
+// Benchmark/+002-4         	 1626250	       733.1 ns/op	      40 B/op	       3 allocs/op
+// Benchmark/+011-4         	 1539116	       754.9 ns/op	      40 B/op	       3 allocs/op
+func Benchmark(b *testing.B) {
+	for _, exp := range []int{-11, 2, 11} {
+		b.Run(fmt.Sprintf("%+04d", exp), func(b *testing.B) {
+			value := 1.234567890 * math.Pow(10, float64(exp))
+			b.ResetTimer()
+
+			for n := 0; n < b.N; n++ {
+				_ = efmt.Sprint(value)
+			}
+		})
+	}
+}
